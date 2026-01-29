031680b0288c27d902c294e227ec7fea17e636d5
Gregor Moe <moethegregor@gmail.com>
2026-01-29 08:21:45 +0100

    bayesisk optimalisering i én dimensjon med test-funksjon

--- optimalisering.py
+++ optimalisering.py
@@ -1,37 +1,58 @@
 import numpy as np
 import scipy.stats
+from sklearn.gaussian_process.kernels import Matern
+from sklearn.gaussian_process import GaussianProcessRegressor
+import matplotlib.pyplot as plt
+import copy
 
-def bayes(observations: np.ndarray, responses: np.ndarray, family):
-    ''' 
-    observations: nxm, 
-    responses: nx1, coverage measure
-    n: different simulations, 
-    m: number of alarms
-    '''
+def loop(x_init, sim_func, acq_func, domain, tol=1e-3, debug=False):
+    X_samples = np.array(x_init).reshape(-1,1)
+    Y_samples = np.array([sim_func(x) for x in x_init])
 
-    posterior = np.exp()
-    return np.argmax(posterior)
-    
-def loop(x0, sim_func: function, acq_func: function, sigma_e):
-    #https://en.wikipedia.org/wiki/Bayesian_optimization#Acquisition_functions
-    
-    #x: grid
-    #score = sim_func(x0)
-    #current_acq = acq_func(x0,score) 
-    #acq_max = Inf
+    history = {'gpr':[],
+               'acq_values': [],
+               'x_next': []}
 
-    #while acq_max > tol:
-        #x_next =  x[argmax(current_acq)]
-        #score = sim_func(x_next)
-    
-        #current_acq = acq_func(x_next,score)
-        #acq_max = max(current_acq_func)
+    gpr = GaussianProcessRegressor(kernel=Matern(nu=2.5))
 
+    acq_max = np.inf
+    while acq_max > tol:
+        gpr.fit(X_samples, Y_samples)
+        history['gpr'].append(copy.deepcopy(gpr))
+        mu, sigma = gpr.predict(domain.reshape(-1,1), return_std=True)
 
-    #need to apply symmetry of fire alarms: (x1,x2,x3) == (x2,x1,x3) osv.
-    return
+        current_acq = acq_func(mu, sigma, np.max(Y_samples))
+        history['acq_values'].append(current_acq)
+
+        idx_next = np.argmax(current_acq)
+        x_next = domain[idx_next]
+        acq_max = current_acq[idx_next]
+        score_next = sim_func(x_next)
+        history['x_next'].append(x_next)
+
+        X_samples = np.vstack((X_samples, [[x_next]]))
+        Y_samples = np.append(Y_samples, score_next)
+        
+        if debug:
+            print(f'x_next: {x_next}\nscore_next:{score_next}')
+    #nshould apply symmetry of fire alarms: (x1,x2,x3) == (x2,x1,x3) osv.
+    return X_samples, Y_samples, history
 
 def expected_improvement(mu_x, sigma_x, f_xplus, xi=0):
     Z = (mu_x - f_xplus - xi)/sigma_x
-    EI = (mu_x - f_xplus - xi)*scipy.stats.norm.cdf(Z) + sigma_x*scipy.stats.normal.pdf(Z)
+    EI = (mu_x - f_xplus - xi)*scipy.stats.norm.cdf(Z) + sigma_x*scipy.stats.norm.pdf(Z)
     return EI
+
+def sim_test(x):
+    return (x-1)**3+1-x
+
+domain = np.arange(0,3,1e-3)
+res_x, res_y, res_history = loop(np.array([0.5]), sim_test, expected_improvement, domain, tol=1e-5)
+print(res_x)
+print(res_y)
+print(f'Maximum at ({res_x[np.argmax(res_y)]}, {max(res_y)}')
+
+plt.scatter(res_x, res_y)
+plt.plot(domain, sim_test(domain))
+plt.show()
+
