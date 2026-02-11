5745cefcc68f47f1e058c99a26a137ef48a3f3a6
Gregor Moe <moethegregor@gmail.com>
2026-02-11 13:42:40 +0100

    3d-optimalisering

--- optimalisering3d.py
+++ optimalisering3d.py
@@ -42,7 +42,7 @@ def loop(x_init, sim_func, acq_func, domain, tol=1e-3, debug=False, save_interva
         Y_samples = np.append(Y_samples, score_next)
         
         if debug:
-            print(f'x_next: {x_next}\nscore_next:{score_next}')
+            print(f'Next point: {x_next}, Function value:{round(score_next,3)}, Acquisition max: {acq_max}')
     #should apply symmetry of fire alarms: (x1,x2,x3) == (x2,x1,x3) osv.
     return X_samples, Y_samples, history
 
@@ -51,27 +51,34 @@ def expected_improvement(mu_x, sigma_x, f_xplus, xi=1e-3): #add another acquisit
     EI = (mu_x - f_xplus - xi)*scipy.stats.norm.cdf(Z) + sigma_x*scipy.stats.norm.pdf(Z)
     return EI
 
-def sim_test2d(x):
-    return (x[0]-1)**3+1-x[0] + (x[1]-1)**2
+def sim_test3d(x):
+    x1, x2, x3 = x[0], x[1], x[2]
+    
+    term1 = 100 * (x2 - x1**2)**2 + (1 - x1)**2
+    term2 = 100 * (x3 - x2**2)**2 + (1 - x2)**2
+    
+    return -(term1 + term2)
 
-def six_hump_camel(x):
-    x1, x2 = x[0], x[1]
-    val = (4 - 2.1*x1**2 + (x1**4)/3)*x1**2 + x1*x2 + (-4 + 4*x2**2)*x2**2
-    return -val
+X, Y, Z = np.meshgrid(np.arange(0,2,1e-2),np.arange(0,2,1e-2),np.arange(0,2,1e-2))
+domain3d = np.vstack([X.ravel(), Y.ravel(), Z.ravel()]).T
 
-X, Y = np.meshgrid(np.arange(-3,3,1e-2),np.arange(-2,2,1e-2))
-domain2d = np.vstack([X.ravel(), Y.ravel()]).T
-
-res_x2d, res_y2d, res_history2d = loop([[0.5, 0.5]], six_hump_camel, expected_improvement, domain2d, tol=.3, debug=True, save_interval=1)
+res_x3d, res_y3d, res_history3d = loop(
+    [[0.5, 0.5, 0.5]], 
+    sim_test3d, 
+    expected_improvement, 
+    domain3d, 
+    tol=.3, 
+    debug=True, 
+    save_interval=1)
 
 data_to_save = {
-    'x': res_x2d,
-    'y': res_y2d,
-    'history': res_history2d,
-    'domain': domain2d
+    'x': res_x3d,
+    'y': res_y3d,
+    'history': res_history3d,
+    'domain': domain3d
 }
 
-with open('bo_results.pkl', 'wb') as f:
+with open('bo_results3d.pkl', 'wb') as f:
     pickle.dump(data_to_save, f)
 
-print("Results saved to bo_results.pkl")
+print("Results saved to bo_results3d.pkl")
